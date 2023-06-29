8f6f0a41eba2a727a12deaee0fb22c882c0f90b2
gardening-y <60008786@naver.com>
2023-06-29 19:30:22 +0900

    prj 생성

---
+++ musicapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+  # path('', index, name='index'),
+  # path('create/',create, name='create'),
+  # path('update/<int:id>',update, name='update'),
+  # path('delete/<int:id>',delete, name='delete'),
+  # path('mypage/<str:author>', mypage, name='mypage'),
+]
